910c16f76273a8d3e2309443a8e45d07c88e6a5d
Artyom <sheludeshev.artyom@mail.ru>
2021-02-23 23:08:18 +0300

    project done

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import { dateFormat } from './utils/DateFormat'
 
-const Countdown = (props) => {
-  const [{ timeString, localeDateString }, setTime] = useState("")
+const Countdown = ({ date }) => {
+  const [{ days, hours, minutes, seconds }, setTime] = useState({})
+  const timeOffsetMoscow = 3
   
   useEffect(() => {
     const intervalID = setInterval(() => {
-      setTime(dateFormat(props.date))
-      console.log(dateFormat(props.date))
+      if (date !== undefined) {
+        const chosenDate = new Date(date)
+        const currentDate = new Date()
+        const delta = chosenDate.getTime() - currentDate.getTime()
+
+        const time = {
+          days: Math.floor(delta / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - timeOffsetMoscow,
+          minutes: Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((delta % (1000 * 60)) / 1000),
+        }
+
+        setTime(time)
+      }
     }, 1000)
 
     return () => clearInterval(intervalID)
-  }, [])
-
-  console.log(props.date)  
+  }, [date])
 
   return (
     <div className="countdown">
-      <span>{ localeDateString }</span>
-      <span>{ timeString }</span>
+      { days === 1 ? <span>{days} day</span> : days > 1 ? <span>{days} days</span> : null}
+      
+      <span>
+        { hours < 10 ? <>0{hours}</> : <>{hours}</> }:
+        { minutes < 10 ? <>0{minutes}</> : <>{minutes}</> }:
+        { seconds < 10 ? <>0{seconds}</> : <>{seconds}</> }
+      </span>
     </div>
   )
 }
